52fb51afc6f64604200c7bab55351794c57a218e
ihateblueb <ihateblueb@proton.me>
2025-06-15 10:41:23 -0400

    initial commit

---
+++ src/main/kotlin/io/github/pylonmc/furniture/PylonFurniture.kt
@@ -0,0 +1,28 @@
+package io.github.pylonmc.furniture
+
+import io.github.pylonmc.pylon.core.addon.PylonAddon
+import lombok.Getter
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
+
+class PylonFurniture : JavaPlugin(), PylonAddon {
+    override fun onEnable() {
+        instance = this
+
+        registerWithPylon()
+    }
+
+    override val javaPlugin: JavaPlugin
+        get() = this
+
+    override val displayName: String
+        get() = "Furniture"
+
+    override val languages: MutableSet<Locale>
+        get() = mutableSetOf(Locale.ENGLISH)
+
+    companion object {
+        @Getter
+        private var instance: PylonFurniture? = null
+    }
+}
